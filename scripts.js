9a5643090455ad20058110646c99e81ebac21c56
stillalvin <amfree8050@gmail.com>
2025-05-30 22:25:58 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Constants
-const BIRTHDAY_DATE = new Date('2025-05-30T22:25:00');
+const BIRTHDAY_DATE = new Date('2025-05-30T22:30:00');
 
 // DOM Elements
 const countdownSection = document.getElementById('countdown-section');
